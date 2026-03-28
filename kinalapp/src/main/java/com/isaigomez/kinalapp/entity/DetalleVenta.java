09ca17be4365740ec27ac7f330b1ca4d60e9a2b7
igomez-2025177 <igomez-2025177@kinal.edu.gt>
2026-03-27 23:05:44 -0600

    feat: agregar atributos, constructores y getters de DetalleVenta

--- kinalapp/src/main/java/com/isaigomez/kinalapp/entity/DetalleVenta.java
+++ kinalapp/src/main/java/com/isaigomez/kinalapp/entity/DetalleVenta.java
@@ -1,4 +1,91 @@
 package com.isaigomez.kinalapp.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "detalle_venta")
 public class DetalleVenta {
+
+    @Id
+    @Column(name = "codigo_detalle_venta")
+    private int codigoDetalleVenta;
+
+    @Column
+    private int cantidad;
+
+    @Column(name = "precio_unitario")
+    private double precioUnitario;
+
+    @Column
+    private double subtotal;
+
+    @Column(name = "codigo_producto")
+    private int codigoProducto;
+
+    @Column(name = "codigo_venta")
+    private int codigoVenta;
+
+    public DetalleVenta() {
+
+    }
+
+    public DetalleVenta(int codigoDetalleVenta, int cantidad, double precioUnitario, double subtotal, int codigoProducto, int codigoVenta) {
+        this.codigoDetalleVenta = codigoDetalleVenta;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.subtotal = subtotal;
+        this.codigoProducto = codigoProducto;
+        this.codigoVenta = codigoVenta;
+    }
+
+    public int getCodigoDetalleVenta() {
+        return codigoDetalleVenta;
+    }
+
+    public void setCodigoDetalleVenta(int codigoDetalleVenta) {
+        this.codigoDetalleVenta = codigoDetalleVenta;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public int getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(int codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public int getCodigoVenta() {
+        return codigoVenta;
+    }
+
+    public void setCodigoVenta(int codigoVenta) {
+        this.codigoVenta = codigoVenta;
+    }
 }
