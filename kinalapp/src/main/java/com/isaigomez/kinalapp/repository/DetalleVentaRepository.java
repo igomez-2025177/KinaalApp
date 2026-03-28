07a85647bd702801aea90f392801d47953844c38
igomez-2025177 <igomez-2025177@kinal.edu.gt>
2026-03-27 23:06:56 -0600

    feat: crear repositorio

---
+++ kinalapp/src/main/java/com/isaigomez/kinalapp/repository/DetalleVentaRepository.java
@@ -0,0 +1,8 @@
+package com.isaigomez.kinalapp.repository;
+
+import com.isaigomez.kinalapp.entity.DetalleVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Integer> {
+
+}
