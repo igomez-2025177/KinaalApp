ded9addf8bff2c3d1eec983a29a34b7934302fe0
igomez-2025177 <igomez-2025177@kinal.edu.gt>
2026-03-27 22:50:59 -0600

    feat: configurar columnas de Venta

--- kinalapp/src/main/java/com/isaigomez/kinalapp/entity/Venta.java
+++ kinalapp/src/main/java/com/isaigomez/kinalapp/entity/Venta.java
@@ -57,7 +57,7 @@ public class Venta {
 
     public void setFechaVenta(Date fechaVenta) {
         this.fechaVenta = fechaVenta;
-    } 
+    }
 
     public double getTotal() {
         return total;
@@ -81,7 +81,7 @@ public class Venta {
 
     public void setDpiCliente(String dpiCliente) {
         this.dpiCliente = dpiCliente;
-    }
+    } 
 
     public int getCodigoUsuario() {
         return codigoUsuario;
