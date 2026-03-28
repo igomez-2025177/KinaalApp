af84594c705f25b07331c63c5852d0644ff54581
igomez-2025177 <igomez-2025177@kinal.edu.gt>
2026-03-27 22:14:41 -0600

    feat: implementar listarTodos en ClienteService

--- kinalapp/src/main/java/com/isaigomez/kinalapp/service/ClienteService.java
+++ kinalapp/src/main/java/com/isaigomez/kinalapp/service/ClienteService.java
@@ -23,7 +23,7 @@ public class ClienteService implements IClienteService{
     /*
     * private: Solo es accesible dentro de la misma clase
     * final: No pude cambiar, es constante
-    * ClienteRepository: El repositorio para acceder a la BD
+    * ClienteRepository: El repositorio para acceder a la BD 
     * Inyeccion de Dependencia ya que Spring nos da el repositorio
      */
     private final ClienteRepository clienteRepository;
