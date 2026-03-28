d98f7ac286695f6228ddbb9b4b494190b21df5f1
igomez-2025177 <igomez-2025177@kinal.edu.gt>
2026-03-27 22:52:10 -0600

    feat: crear interfaz de servicio Venta

--- kinalapp/src/main/java/com/isaigomez/kinalapp/repository/VentaRepository.java
+++ kinalapp/src/main/java/com/isaigomez/kinalapp/repository/VentaRepository.java
@@ -1,4 +1,8 @@
 package com.isaigomez.kinalapp.repository;
 
-public interface VentaRepository {
+import com.isaigomez.kinalapp.entity.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Integer> {
+
 }
