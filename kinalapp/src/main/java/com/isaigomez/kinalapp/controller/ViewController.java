d1fb471263cb505322831b80f4ba9a383afeed31
igomez-2025177 <igomez-2025177@kinal.edu.gt>
2026-04-14 14:30:54 -0600

    feat: cree controlador de vistas

---
+++ kinalapp/src/main/java/com/isaigomez/kinalapp/controller/ViewController.java
@@ -0,0 +1,38 @@
+package com.isaigomez.kinalapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String inicio() {
+        return "index";
+    }
+
+    @GetMapping("/clientes-view")
+    public String clientes() {
+        return "clientes";
+    }
+
+    @GetMapping("/usuarios-view")
+    public String usuarios() {
+        return "usuarios";
+    }
+
+    @GetMapping("/productos-view")
+    public String productos() {
+        return "productos";
+    }
+
+    @GetMapping("/ventas-view")
+    public String ventas() {
+        return "ventas";
+    }
+
+    @GetMapping("/detalleventa-view")
+    public String detalleVenta() {
+        return "detalleventa";
+    }
+}
